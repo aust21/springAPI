44dda6e3c7d60d794503224d95394eed672214a8
Austin Ngobeni <kngobeni223@gmail.com>
2025-01-23 14:14:45 +0200

    add files

---
+++ src/main/java/com/enviro/assessment/grad001/austinngobeni/model/WasteCategories.java
@@ -0,0 +1,18 @@
+package com.enviro.assessment.grad001.austinngobeni.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "categories")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class WasteCategories {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String disposal;
+}
