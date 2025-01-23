4aaac0b6dc1d32bfd1cc948289ef745a81c8789b
Austin Ngobeni <kngobeni223@gmail.com>
2025-01-23 19:14:20 +0200

    complete update

--- src/main/java/com/enviro/assessment/grad001/austinngobeni/controller/GuidelineController.java
+++ src/main/java/com/enviro/assessment/grad001/austinngobeni/controller/GuidelineController.java
@@ -1,24 +1,35 @@
 package com.enviro.assessment.grad001.austinngobeni.controller;
 
 import com.enviro.assessment.grad001.austinngobeni.model.Guidelines;
+import com.enviro.assessment.grad001.austinngobeni.repo.GuideLineRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/guide")
 public class GuidelineController implements ControllerInterface<Guidelines>{
 
     @Autowired
-    private GuidelineController guideController;
+    private GuideLineRepo guideRepo;
 
     @Override
     @PutMapping("/update/{id}")
     public ResponseEntity<Guidelines> updateById(Long id, Guidelines body) {
-        return null;
+        Optional<Guidelines> existingData = guideRepo.findById(id);
+
+        if (existingData.isPresent()) {
+            Guidelines updatedData = existingData.get();
+            updatedData.setGuideline(body.getGuideline());
+            Guidelines newObj = guideRepo.save(updatedData);
+            return new ResponseEntity<>(newObj, HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @Override
