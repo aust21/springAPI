4998dd39d55f0d144e498c880bf068824c5eb821
Austin Ngobeni <kngobeni223@gmail.com>
2025-01-23 16:13:42 +0200

    implement updateById

--- src/main/java/com/enviro/assessment/grad001/austinngobeni/controller/CategoryController.java
+++ src/main/java/com/enviro/assessment/grad001/austinngobeni/controller/CategoryController.java
@@ -3,21 +3,29 @@ package com.enviro.assessment.grad001.austinngobeni.controller;
 import com.enviro.assessment.grad001.austinngobeni.model.WasteCategories;
 import com.enviro.assessment.grad001.austinngobeni.repo.WasteCategoryRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.Optional;
+
 public class CategoryController implements ControllerInterface<WasteCategories>{
 
     @Autowired
-    private WasteCategoryRepo wasteCategories;
+    private WasteCategoryRepo categoryRepo;
 
     @Override
     @PutMapping("/cat?id={id}")
     public ResponseEntity<WasteCategories> updateById(@PathVariable Long id) {
-        return null;
+        Optional<WasteCategories> data = categoryRepo.findById(id);
+
+        if (data.isPresent()) {
+            return new ResponseEntity<>(data.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @Override
@@ -38,7 +46,7 @@ public class CategoryController implements ControllerInterface<WasteCategories>{
     @Override
     @PostMapping("/cat/add")
     public ResponseEntity<WasteCategories> addData(@RequestBody WasteCategories category) {
-        WasteCategories savedCategory = wasteCategories.save(category);
+        WasteCategories savedCategory = categoryRepo.save(category);
         return ResponseEntity.ok(savedCategory);
     }
 }
